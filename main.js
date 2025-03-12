f934fca3c1d6d4ef345accf338bbfd900a32548a
yousof <enderglitcher123@gmail.comm>
2025-03-13 00:10:48 +0300

    added delete option to the cart

--- main.js
+++ main.js
@@ -12,23 +12,39 @@ btnHeart.style.color ='black'
 let cartList = document.querySelector('#cart-list');
 let cartTitle = document.querySelector('#cart-title');
 let purshaseBtn = document.querySelector('#purchase-btn');
+
 let canRun = true
+
 
 
 document.querySelector("#product-area").addEventListener('click', function(e){
     if(e.target.getAttribute('class') == addBtn.getAttribute('class') && canRun == true){
         addNumToCart()
         cartNav.innerHTML = '<i class="fa-solid fa-cart-shopping"></i>'+'view cart'+"(" +cartNum+")";
-        let p = document.createElement('p')
-        let li = document.createElement('li')
+        let p = document.createElement('p');
+        let li = document.createElement('li');
+        let divClose = document.createElement('div')
+        divClose.setAttribute('class', 'list-close-btn');
+        divClose.innerHTML = '&times;'
         p.innerText = 'Coming Soon!'
         li.setAttribute('class', 'li-cart')
-        li.append(p)
+        li.append(p, divClose)
         cartList.append(li)
         cartTitle.style.display = 'none'
         popupContainer.style.display = 'block'
         purshaseBtn.style.display = 'block'
         maxCartNum()
+        divClose.addEventListener('click', function(){
+            li.remove();
+            cartNum -=1
+            cartNav.innerHTML = '<i class="fa-solid fa-cart-shopping"></i>'+'view cart'+"(" +cartNum+")";
+            if (cartNum == 0){
+                cartTitle.style.display = 'block'
+                purshaseBtn.style.display = 'none'
+
+            }
+
+        })
 
     }
 })
@@ -45,6 +61,7 @@ closeBtnPop.addEventListener('click', function(){
 closeBtnCart.addEventListener('click', function(){
     modalCartContainer.style.display = 'none'
 })
+
 
 function addNumToCart(){
     cartNum += 1
